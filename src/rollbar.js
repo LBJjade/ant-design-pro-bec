9d54c517626a1f3d42efeb8a57e696535ee1c3bc
hequnmin <min@becheer.com>
2018-01-06 12:26:21 +0800

    修改网络资源配置

--- src/rollbar.js
+++ src/rollbar.js
@@ -1,13 +1,24 @@
 import Rollbar from 'rollbar';
 
 // Track error by https://sentry.io/
-if (location.host === 'preview.pro.ant.design') {
-  Rollbar.init({
-    accessToken: '033ca6d7c0eb4cc1831cf470c2649971',
-    captureUncaught: true,
-    captureUnhandledRejections: true,
-    payload: {
-      environment: 'production',
-    },
-  });
-}
+// if (location.host === 'preview.pro.ant.design') {
+//   Rollbar.init({
+//     accessToken: '033ca6d7c0eb4cc1831cf470c2649971',
+//     captureUncaught: true,
+//     captureUnhandledRejections: true,
+//     payload: {
+//       environment: 'production',
+//     },
+//   });
+// }
+
+// Track error by https://sentry.io/
+// hequnmin@gmail.com
+Rollbar.init({
+  accessToken: '287e78bef05711e781fd4201c0a8d032',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+  payload: {
+    environment: 'development',
+  },
+});
