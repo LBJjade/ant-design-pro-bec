f8f2b59c0e16794111d8f72ce5495912c68778b1
lbj <2105931798@qq.com>
2018-03-07 18:58:42 +0800

    1.大区表单（基本）品牌选择按钮（checkbox）

---
+++ src/routes/SysSet/BigAreaTableList.js
@@ -0,0 +1,479 @@
+import React, { PureComponent } from 'react';
+import { connect } from 'dva';
+import moment from 'moment';
+import { Row, Col, Card, Form, a, Input, InputNumber, Select, Icon, Button, Dropdown, Menu, DatePicker, Modal, message, Table } from 'antd';
+import PageHeaderLayout from '../../layouts/PageHeaderLayout';
+
+import styles from './BigAreaTableList.less';
+
+const FormItem = Form.Item;
+const { Option } = Select;
+const getValue = obj => Object.keys(obj).map(key => obj[key]).join(',');
+const columns = [
+  {
+    title: '序号',
+    dataIndex: 'orderNumber',
+  },
+  {
+    title: '品牌名称',
+    dataIndex: 'brandName',
+  },
+  {
+    title: '操作',
+    dataIndex: 'operate',
+    render: val => <span><a href="brandManage/fetch">{val}</a></span>,
+  },
+  {
+    title: '更新时间',
+    dataIndex: 'updatedAt',
+    sorter: true,
+    render: val => <span>{moment(val).format('YYYY-MM-DD HH:mm:ss')}</span>,
+  }, {
+    title: '创建时间',
+    dataIndex: 'createdAt',
+    sorter: true,
+    render: val => <span>{moment(val).format('YYYY-MM-DD HH:mm:ss')}</span>,
+  },
+];
+
+const rowSelection = {
+  onChange: (selectedRowKeys, selectedRows) => {
+    console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
+  },
+  getCheckboxProps: record => ({
+    disabled: record.name === 'Disabled User', // Column configuration not to be checked
+    name: record.name,
+  }),
+};
+
+const CreateAddForm = Form.create()((props) => {
+  const { modalVisible, form, handleAdd, handleModalVisible } = props;
+  const okHandle = () => {
+    form.validateFields((err, fieldsValue) => {
+      if (err) return;
+      handleAdd(fieldsValue);
+    });
+  };
+  return (
+    <Modal
+      title="新增"
+      visible={modalVisible}
+      onOk={okHandle}
+      onCancel={() => handleModalVisible()}
+    >
+      <FormItem
+        labelCol={{ span: 5 }}
+        wrapperCol={{ span: 15 }}
+        label="序号"
+      >
+        {form.getFieldDecorator('orderNumber', {
+          rules: [{ required: true, message: '请输入序号...' }],
+        })(
+          <InputNumber placeholder="请输入" />
+        )}
+      </FormItem>
+      <FormItem
+        labelCol={{ span: 5 }}
+        wrapperCol={{ span: 15 }}
+        label="品牌名称"
+      >
+        {form.getFieldDecorator('brandName', {
+          rules: [{ required: true, message: '请输入品牌名称...' }],
+        })(
+          <Input placeholder="请输入" />
+        )}
+      </FormItem>
+    </Modal>
+  );
+});
+
+const CreateEditForm = Form.create()((props) => {
+  const { modalEditVisible, form, handleAdd, handleModalVisible } = props;
+  const okHandle = () => {
+    form.validateFields((err, fieldsValue) => {
+      if (err) return;
+      handleAdd(fieldsValue);
+    });
+  };
+  return (
+    <Modal
+      title="编辑"
+      visible={modalEditVisible}
+      onOk={okHandle}
+      onCancel={() => handleModalVisible()}
+    >
+      <FormItem
+        labelCol={{ span: 5 }}
+        wrapperCol={{ span: 15 }}
+        label="序号"
+      >
+        {form.getFieldDecorator('orderNumber', {
+          rules: [{ required: true, message: '请输入序号...' }],
+        })(
+          <InputNumber placeholder="请输入" />
+        )}
+      </FormItem>
+      <FormItem
+        labelCol={{ span: 5 }}
+        wrapperCol={{ span: 15 }}
+        label="品牌名称"
+      >
+        {form.getFieldDecorator('brandName', {
+          rules: [{ required: true, message: '请输入品牌名称...' }],
+        })(
+          <Input placeholder="请输入" />
+        )}
+      </FormItem>
+    </Modal>
+  );
+});
+
+
+@connect(({ brandManage, loading }) => ({
+  brandManage,
+  loading: loading.models.brandManage,
+}))
+@Form.create()
+export default class TableList extends PureComponent {
+  state = {
+    pagination: {
+      pageSize: 5,
+      current: 1,
+      total: 0,
+    },
+    modalVisible: false,
+    modalEditVisible: false,
+    expandForm: false,
+    selectedRows: [],
+    formValues: {},
+  };
+
+  componentDidMount() {
+    const { dispatch } = this.props;
+    dispatch({
+      type: 'brandManage/fetch',
+    });
+  }
+
+  handleStandardTableChange = (pagination, filtersArg, sorter) => {
+    const { dispatch } = this.props;
+    const { formValues } = this.state;
+
+    const filters = Object.keys(filtersArg).reduce((obj, key) => {
+      const newObj = { ...obj };
+      newObj[key] = getValue(filtersArg[key]);
+      return newObj;
+    }, {});
+
+    const params = {
+      skip: ((pagination.current - 1) * pagination.pageSize) + 1,
+      limit: pagination.pageSize,
+      count: true,
+      ...formValues,
+      ...filters,
+    };
+    if (sorter.field) {
+      params.sorter = `${sorter.field}_${sorter.order}`;
+    }
+
+    dispatch({
+      type: 'brandManage/fetch',
+      payload: params,
+    });
+    this.setState({
+      pagination: {
+        current: pagination.current,
+        pageSize: pagination.pageSize,
+      },
+    });
+  }
+
+  handleFormAdd = () => {
+    const { form, dispatch } = this.props;
+    form.resetFields();
+    this.setState({
+      formValues: {},
+    });
+    dispatch({
+      type: 'brandManage/fetch',
+      payload: {},
+    });
+  }
+
+  toggleForm = () => {
+    this.setState({
+      expandForm: !this.state.expandForm,
+    });
+  }
+
+  handleMenuClick = (e) => {
+    const { dispatch } = this.props;
+    const { selectedRows } = this.state;
+
+    if (!selectedRows) return;
+
+    switch (e.key) {
+      case 'remove':
+        dispatch({
+          type: 'brandManage/remove',
+          payload: {
+            no: selectedRows.map(row => row.no).join(','),
+          },
+          callback: () => {
+            this.setState({
+              selectedRows: [],
+            });
+          },
+        });
+        break;
+      default:
+        break;
+    }
+  }
+
+  handleSelectRows = (rows) => {
+    this.setState({
+      selectedRows: rows,
+    });
+  }
+
+  handleSearch = (e) => {
+    e.preventDefault();
+
+    const { dispatch, form } = this.props;
+
+    form.validateFields((err, fieldsValue) => {
+      if (err) return;
+
+      const values = {
+        ...fieldsValue,
+        updatedAt: fieldsValue.updatedAt && fieldsValue.updatedAt.valueOf(),
+      };
+
+      this.setState({
+        formValues: values,
+      });
+
+      dispatch({
+        type: 'brandManage/fetch',
+        payload: values,
+      });
+    });
+  }
+
+  handleAddModalVisible = (flag) => {
+    this.setState({
+      modalVisible: !!flag,
+    });
+  }
+
+  handleEditModalVisible = (flag) => {
+    this.setState({
+      modalEditVisible: !!flag,
+    });
+  }
+
+  handleAdd = (fields) => {
+    this.props.dispatch({
+      type: 'brandManage/add',
+      payload: fields,
+    });
+
+    message.success('添加成功');
+    this.setState({
+      modalVisible: false,
+    });
+  }
+
+  handleEdit = (key) => {
+    this.props.dispatch({
+      type: 'brandManage/edit',
+      payload: key,
+    });
+
+    message.success('添加成功');
+    this.setState({
+      modalEditVisible: false,
+    });
+  }
+
+  renderSimpleForm() {
+    const { getFieldDecorator } = this.props.form;
+    return (
+      <Form onSubmit={this.handleSearch} layout="inline">
+        <Row gutter={{ md: 8, lg: 24, xl: 48 }}>
+          <Col md={8} sm={24}>
+            <FormItem label="序号">
+              {getFieldDecorator('no')(
+                <Input placeholder="请输入" />
+              )}
+            </FormItem>
+          </Col>
+          <Col md={8} sm={24}>
+            <FormItem label="品牌名称">
+              {getFieldDecorator('brandName')(
+                <Select placeholder="请选择" style={{ width: '100%' }}>
+                  <Option value="0">关闭</Option>
+                  <Option value="1">运行中</Option>
+                </Select>
+              )}
+            </FormItem>
+          </Col>
+          <Col md={8} sm={24}>
+            <span className={styles.submitButtons}>
+              <Button type="primary" htmlType="submit">查询</Button>
+              <Button style={{ marginLeft: 8 }} onClick={this.handleFormAdd}>刷新</Button>
+              <a style={{ marginLeft: 8 }} onClick={this.toggleForm}>
+                展开 <Icon type="down" />
+              </a>
+            </span>
+          </Col>
+        </Row>
+      </Form>
+    );
+  }
+
+  renderAdvancedForm() {
+    const { getFieldDecorator } = this.props.form;
+    return (
+      <Form onSubmit={this.handleSearch} layout="inline">
+        <Row gutter={{ md: 8, lg: 24, xl: 48 }}>
+          <Col md={8} sm={24}>
+            <FormItem label="序号">
+              {getFieldDecorator('no')(
+                <Input placeholder="请输入" />
+              )}
+            </FormItem>
+          </Col>
+          <Col md={8} sm={24}>
+            <FormItem label="品牌名称">
+              {getFieldDecorator('brandName')(
+                <Select placeholder="请选择" style={{ width: '100%' }}>
+                  <Option value="0">关闭</Option>
+                  <Option value="1">运行中</Option>
+                </Select>
+              )}
+            </FormItem>
+          </Col>
+          <Col md={8} sm={24}>
+            <FormItem label="更新日期">
+              {getFieldDecorator('updatedAt')(
+                <DatePicker style={{ width: '100%' }} placeholder="请输入更新日期" />
+              )}
+            </FormItem>
+          </Col>
+        </Row>
+        <Row gutter={{ md: 8, lg: 24, xl: 48 }}>
+          <Col md={8} sm={24}>
+            <FormItem label="创建时间">
+              {getFieldDecorator('createdAt')(
+                <DatePicker style={{ width: '100%' }} placeholder="请输入创建时间" />
+              )}
+            </FormItem>
+          </Col>
+          <Col md={8} sm={24}>
+            <span className={styles.submitButtons}>
+              <Button type="primary" htmlType="submit">查询</Button>
+              <Button style={{ marginLeft: 8 }} onClick={this.handleFormAdd}>刷新</Button>
+              <a style={{ marginLeft: 8 }} onClick={this.toggleForm}>
+              收起 <Icon type="up" />
+              </a>
+            </span>
+          </Col>
+        </Row>
+      </Form>
+    );
+  }
+
+  renderForm() {
+    return this.state.expandForm ? this.renderAdvancedForm() : this.renderSimpleForm();
+  }
+
+  render() {
+    const { brandManage: { data }, loading } = this.props;
+    const { selectedRows, modalVisible, modalEditVisible } = this.state;
+
+    const menu = (
+      <Menu onClick={this.handleMenuClick} selectedKeys={[]}>
+        <Menu.Item key="remove">删除</Menu.Item>
+        <Menu.Item key="approval">批量审批</Menu.Item>
+      </Menu>
+    );
+
+    const parentAddMethods = {
+      handleAdd: this.handleAdd,
+      handleModalVisible: this.handleAddModalVisible,
+    };
+
+    const parentEditMethods = {
+      handleAdd: this.handleAdd,
+      handleModalVisible: this.handleEditModalVisible,
+    };
+
+    const paginationProps = {
+      showSizeChanger: true,
+      showQuickJumper: true,
+      pageSize: this.state.pagination.pageSize,
+      total: data.count,
+      // onChange: this.handlePageChange,
+    };
+
+    return (
+      <PageHeaderLayout title="品牌管理">
+        <Card bordered={false}>
+          <div className={styles.tableList}>
+            <div className={styles.tableListForm}>
+              {this.renderForm()}
+            </div>
+            <div className={styles.tableListOperator}>
+              <Button icon="plus" type="primary" onClick={() => this.handleAddModalVisible(true)}>
+                新增
+              </Button>
+              <Button icon="edit" type="primary" onClick={() => this.handleEditModalVisible(true)}>
+                编辑
+              </Button>
+              <Button icon="delete" type="primary" onClick={() => this.handleModalVisible()}>
+                删除
+              </Button>
+              {
+                selectedRows.length > 0 && (
+                  <span>
+                    <Button>批量操作</Button>
+                    <Dropdown overlay={menu}>
+                      <Button>
+                        更多操作 <Icon type="down" />
+                      </Button>
+                    </Dropdown>
+                  </span>
+                )
+              }
+            </div>
+            <div className={styles.standardList}>
+              <Card>
+                <div>
+                  <Table
+                    rowKey="objectId"
+                    columns={columns}
+                    loading={loading}
+                    pagination={paginationProps}
+                    dataSource={data.results}
+                    onChange={this.handleStandardTableChange}
+                    rowSelection={rowSelection}
+                  />
+                </div>
+              </Card>
+            </div>
+          </div>
+        </Card>
+        <CreateAddForm
+          {...parentAddMethods}
+          modalVisible={modalVisible}
+        />
+        <CreateEditForm
+          {...parentEditMethods}
+          modalEditVisible={modalEditVisible}
+        />
+      </PageHeaderLayout>
+    );
+  }
+}
