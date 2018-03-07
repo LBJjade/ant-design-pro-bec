9e2a4fabd6d12752fe26bb6f79d095176d6044c4
lbj <2105931798@qq.com>
2018-03-07 19:01:23 +0800

    1.大区表单（基本）品牌选择按钮（checkbox）

--- src/routes/SysSet/TableList.js
+++ src/routes/SysSet/TableList.js
@@ -1,7 +1,7 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'dva';
 import moment from 'moment';
-import { Row, Col, Card, Form, Input, InputNumber, Select, Icon, Button, Dropdown, Menu, DatePicker, Modal, message, Table } from 'antd';
+import { Row, Col, Card, Form, a, Input, InputNumber, Select, Icon, Button, Dropdown, Menu, DatePicker, Modal, message, Table } from 'antd';
 import PageHeaderLayout from '../../layouts/PageHeaderLayout';
 
 import styles from './TableList.less';
@@ -21,6 +21,7 @@ const columns = [
   {
     title: '操作',
     dataIndex: 'operate',
+    render: val => <span><a href="brandManage/fetch">{val}</a></span>,
   },
   {
     title: '更新时间',
@@ -35,7 +36,17 @@ const columns = [
   },
 ];
 
-const CreateForm = Form.create()((props) => {
+const rowSelection = {
+  onChange: (selectedRowKeys, selectedRows) => {
+    console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
+  },
+  getCheckboxProps: record => ({
+    disabled: record.name === 'Disabled User', // Column configuration not to be checked
+    name: record.name,
+  }),
+};
+
+const CreateAddForm = Form.create()((props) => {
   const { modalVisible, form, handleAdd, handleModalVisible } = props;
   const okHandle = () => {
     form.validateFields((err, fieldsValue) => {
@@ -47,6 +58,47 @@ const CreateForm = Form.create()((props) => {
     <Modal
       title="新增"
       visible={modalVisible}
+      onOk={okHandle}
+      onCancel={() => handleModalVisible()}
+    >
+      <FormItem
+        labelCol={{ span: 5 }}
+        wrapperCol={{ span: 15 }}
+        label="序号"
+      >
+        {form.getFieldDecorator('orderNumber', {
+          rules: [{ required: true, message: '请输入序号...' }],
+        })(
+          <InputNumber placeholder="请输入" />
+        )}
+      </FormItem>
+      <FormItem
+        labelCol={{ span: 5 }}
+        wrapperCol={{ span: 15 }}
+        label="品牌名称"
+      >
+        {form.getFieldDecorator('brandName', {
+          rules: [{ required: true, message: '请输入品牌名称...' }],
+        })(
+          <Input placeholder="请输入" />
+        )}
+      </FormItem>
+    </Modal>
+  );
+});
+
+const CreateEditForm = Form.create()((props) => {
+  const { modalEditVisible, form, handleAdd, handleModalVisible } = props;
+  const okHandle = () => {
+    form.validateFields((err, fieldsValue) => {
+      if (err) return;
+      handleAdd(fieldsValue);
+    });
+  };
+  return (
+    <Modal
+      title="编辑"
+      visible={modalEditVisible}
       onOk={okHandle}
       onCancel={() => handleModalVisible()}
     >
@@ -90,6 +142,7 @@ export default class TableList extends PureComponent {
       total: 0,
     },
     modalVisible: false,
+    modalEditVisible: false,
     expandForm: false,
     selectedRows: [],
     formValues: {},
@@ -208,9 +261,15 @@ export default class TableList extends PureComponent {
     });
   }
 
-  handleModalVisible = (flag) => {
+  handleAddModalVisible = (flag) => {
     this.setState({
       modalVisible: !!flag,
+    });
+  }
+
+  handleEditModalVisible = (flag) => {
+    this.setState({
+      modalEditVisible: !!flag,
     });
   }
 
@@ -223,6 +282,18 @@ export default class TableList extends PureComponent {
     message.success('添加成功');
     this.setState({
       modalVisible: false,
+    });
+  }
+
+  handleEdit = (key) => {
+    this.props.dispatch({
+      type: 'brandManage/edit',
+      payload: key,
+    });
+
+    message.success('添加成功');
+    this.setState({
+      modalEditVisible: false,
     });
   }
 
@@ -303,7 +374,7 @@ export default class TableList extends PureComponent {
           <Col md={8} sm={24}>
             <span className={styles.submitButtons}>
               <Button type="primary" htmlType="submit">查询</Button>
-              <Button style={{ marginLeft: 8 }} onClick={this.handleFormAdd}>新增</Button>
+              <Button style={{ marginLeft: 8 }} onClick={this.handleFormAdd}>刷新</Button>
               <a style={{ marginLeft: 8 }} onClick={this.toggleForm}>
               收起 <Icon type="up" />
               </a>
@@ -320,7 +391,7 @@ export default class TableList extends PureComponent {
 
   render() {
     const { brandManage: { data }, loading } = this.props;
-    const { selectedRows, modalVisible } = this.state;
+    const { selectedRows, modalVisible, modalEditVisible } = this.state;
 
     const menu = (
       <Menu onClick={this.handleMenuClick} selectedKeys={[]}>
@@ -329,9 +400,14 @@ export default class TableList extends PureComponent {
       </Menu>
     );
 
-    const parentMethods = {
+    const parentAddMethods = {
       handleAdd: this.handleAdd,
-      handleModalVisible: this.handleModalVisible,
+      handleModalVisible: this.handleAddModalVisible,
+    };
+
+    const parentEditMethods = {
+      handleAdd: this.handleAdd,
+      handleModalVisible: this.handleEditModalVisible,
     };
 
     const paginationProps = {
@@ -350,8 +426,14 @@ export default class TableList extends PureComponent {
               {this.renderForm()}
             </div>
             <div className={styles.tableListOperator}>
-              <Button icon="plus" type="primary" onClick={() => this.handleModalVisible(true)}>
+              <Button icon="plus" type="primary" onClick={() => this.handleAddModalVisible(true)}>
                 新增
+              </Button>
+              <Button icon="edit" type="primary" onClick={() => this.handleEditModalVisible(true)}>
+                编辑
+              </Button>
+              <Button icon="delete" type="primary" onClick={() => this.handleModalVisible()}>
+                删除
               </Button>
               {
                 selectedRows.length > 0 && (
@@ -370,20 +452,26 @@ export default class TableList extends PureComponent {
               <Card>
                 <div>
                   <Table
+                    rowKey="objectId"
                     columns={columns}
                     loading={loading}
                     pagination={paginationProps}
                     dataSource={data.results}
                     onChange={this.handleStandardTableChange}
+                    rowSelection={rowSelection}
                   />
                 </div>
               </Card>
             </div>
           </div>
         </Card>
-        <CreateForm
-          {...parentMethods}
+        <CreateAddForm
+          {...parentAddMethods}
           modalVisible={modalVisible}
+        />
+        <CreateEditForm
+          {...parentEditMethods}
+          modalEditVisible={modalEditVisible}
         />
       </PageHeaderLayout>
     );
