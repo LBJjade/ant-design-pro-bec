6b988eca7145d063dfcc364a3e5e55d72c0bee21
lbj <2105931798@qq.com>
2018-05-03 14:37:36 +0800

    1.详情页面调整

--- src/routes/Personal/Detail.js
+++ src/routes/Personal/Detail.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'dva';
 import moment from 'moment';
-import { Card, Avatar, Form, Tag, Spin, Table } from 'antd';
+import { Avatar, Tag, Spin, Table, Row, Col } from 'antd';
 import PageHeaderLayout from '../../layouts/PageHeaderLayout';
 
 @connect(({ information, loading }) => ({
@@ -79,6 +79,36 @@ export default class BasicProfile extends Component {
         {
           this.state.loading ? <div style={{ width: '100%', height: '100%' }}><Spin /></div> : (
             <PageHeaderLayout title="信息详情页">
+              <div style={{ height: 'auto', marginLeft: '400px', marginTop: '20px' }}>
+                <div style={{ margin: 8 }}>
+                  <Row>
+                    <Col span={4}>头像:</Col>
+                    <Col span={8}><Avatar src={vaule === undefined ? '' : (vaule.avatar === undefined ? 'https://gw.alipayobjects.com/zos/rmsportal/kISTdvpyTAhtGxpovNWd.png' : vaule.avatar)} shape="square" size="large" /></Col>
+                  </Row>
+                </div>
+                <br />
+                <div style={{ margin: 8 }}>
+                  <Row>
+                    <Col span={4}>信息标题:</Col>
+                    <Col span={8}>{vaule === undefined ? '' : (vaule.title === undefined ? '暂无' : vaule.title)}</Col>
+                  </Row>
+                </div>
+                <br />
+                <div style={{ margin: 8 }}>
+                  <Row>
+                    <Col span={4}>信息详情:</Col>
+                    <Col span={8}><Tag color={vaule === undefined ? '' : color[`${vaule.status}`]} style={{ marginRight: 0 }}>{vaule === undefined ? '' : (vaule.extra === undefined ? '暂无' : vaule.extra)}</Tag></Col>
+                  </Row>
+                </div>
+                <br />
+                <div style={{ margin: 8 }}>
+                  <Row>
+                    <Col span={4}>时间:</Col>
+                    <Col span={8}>{vaule === undefined ? '' : vaule.createdAt}</Col>
+                  </Row>
+                </div>
+              </div>
+              <div style={{ height: 100 }} />
               <Table
                 style={{ marginBottom: 24 }}
                 pagination={false}
@@ -86,38 +116,6 @@ export default class BasicProfile extends Component {
                 columns={columns}
                 rowKey="id"
               />
-              <Card bordered={false}>
-                <Form
-                  hideRequiredMark
-                  style={{ marginTop: 8 }}
-                >
-                  <Form.Item
-                    label="头像"
-                  >
-                    <Avatar src={vaule === undefined ? '' : (vaule.avatar === undefined ? 'https://gw.alipayobjects.com/zos/rmsportal/kISTdvpyTAhtGxpovNWd.png' : vaule.avatar)} shape="square" size="large" />
-                  </Form.Item>
-                  <Form.Item
-                    label="信息标题"
-                  >
-                    {vaule === undefined ? '' : (vaule.title === undefined ? '暂无' : vaule.title)}
-                  </Form.Item>
-                  <Form.Item
-                    label="信息详情"
-                  >
-                    {vaule === undefined ? '' : (vaule.description === undefined ? '暂无' : vaule.description)}
-                  </Form.Item>
-                  <Form.Item
-                    label="特别说明"
-                  >
-                    <Tag color={vaule === undefined ? '' : color[`${vaule.status}`]} style={{ marginRight: 0 }}>{vaule === undefined ? '' : (vaule.extra === undefined ? '暂无' : vaule.extra)}</Tag>
-                  </Form.Item>
-                  <Form.Item
-                    label="时间"
-                  >
-                    {vaule === undefined ? '' : vaule.createdAt}
-                  </Form.Item>
-                </Form>
-              </Card>
             </PageHeaderLayout>
 )}
       </div>
